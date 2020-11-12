8725de2704fac74e087c745946fbcdab3f9b4d0c
Abdallah-Nazzal <57283926+Abdallah-Nazzal@users.noreply.github.com>
2020-11-12 09:46:21 +0200

    Update ml.py

--- ml.py
+++ ml.py
@@ -57,5 +57,5 @@ def predict_house_price():
 if __name__ == '__main__':
     
 
-    app.run(debug=True, use_reloader=False,host ='0.0.0.0', port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, use_reloader=False,host ='0.0.0.0', port = int(os.environ.get('PORT', 8000))
 
